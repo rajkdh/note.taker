c2d16a5a8aa9f31a3dd82dbf02d7529619252d19
rajkdh <112664790+rajkdh@users.noreply.github.com>
2023-05-29 01:39:13 -0400

    removed comments, cleaned code for better readability

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-// add dependecy variables
 const express = require("express");
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
-//init app and creat port
 const app = express();
-
 const PORT = process.env.PORT || 8080;
-// set up body parcing, static, and route middleware
+
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
-//start tje server on the port
+
+
 app.listen(PORT, () => {
     console.log(`Now listening on PORT: ${PORT}`);
 });
