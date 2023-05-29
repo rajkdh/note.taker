f542d52f1afe680239c25d4f482c30b4569ed1d8
rajkdh <112664790+rajkdh@users.noreply.github.com>
2023-05-28 23:52:35 -0400

    added code to GET stored data and convert to json

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,12 @@
 const router = require("express").Router();
 const store = require("../db/store");
+
+//call for stored data
+router.get("/notes", (req, res) => {
+    store
+    .getNotes().then(notes => {
+        res.json(notes)
+    }).catch(err => {
+        res.status(500).json(err)
+    })
+})
