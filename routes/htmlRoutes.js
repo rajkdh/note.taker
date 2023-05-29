d1ed486db4423fca89e8f36a9dfec37028310623
rajkdh <112664790+rajkdh@users.noreply.github.com>
2023-05-29 01:18:21 -0400

    removed comments

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const router = require('express').Router();
 
-//set route - note data sent to html file
 router.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'))
 })
