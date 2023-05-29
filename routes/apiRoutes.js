67bd2c1eb26624a581649ba777d2c9b173f55944
rajkdh <112664790+rajkdh@users.noreply.github.com>
2023-05-28 23:55:41 -0400

    added code to post input note to store file in json format

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,3 +10,13 @@ router.get("/notes", (req, res) => {
         res.status(500).json(err)
     })
 })
+
+//post notes
+router.post("/notes", (req, res) => {
+    store
+    .addNote(req.body).then(note => {
+        res.json(note)
+    }).catch(err => {
+        res.status(500).json(err)
+    })
+})
